35903eddcd894b54b19dfd6d2a4c973dff8cbafd
Ben <benjisgarza@gmail.com>
2020-02-06 10:18:19 -0700

    Adding tests for calculator function

--- testing/functions.test.js
+++ testing/functions.test.js
@@ -1,5 +1,6 @@
 const capitalizeString = require('../src/capitalizeString')
 const reverseString = require('../src/reverseString')
+const calculator = require('../src/calculator')
 
 test('should Capitalize first letter', () => {
   expect(capitalizeString('string')).toBe('String')
@@ -7,4 +8,17 @@ test('should Capitalize first letter', () => {
 
 test('should reverse string', () => {
   expect(reverseString('string')).toBe('gnirts')
+})
+
+test('should return 4', () => {
+  expect(calculator.add(2, 2)).toBe(4)
+})
+test('should return 0', () => {
+  expect(calculator.subtract(2, 2)).toBe(0)
+})
+test('should return 4', () => {
+  expect(calculator.multiple(2, 2)).toBe(4)
+})
+test('should return 1', () => {
+  expect(calculator.divide(2, 2)).toBe(1)
 })
